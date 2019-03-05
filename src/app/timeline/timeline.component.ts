0f880e1d4bd10410f7a8073338f2765d7ac1b187
Letonade <cocoscorpio94@hotmail.fr>
2019-03-05 15:47:14 +0100

    My On work version

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -7,24 +7,44 @@ import { Tweet } from '../tweet';
   styleUrls: ['./timeline.component.less']
 })
 export class TimelineComponent implements OnInit {
+
+  user : string = 'ME'
+
   tweets : Tweet[] = [
      {
       created_at: 'Wed Apr 05 12:30:12 +0000 2017',
       id: 1,
       text: 'Je mets les pieds où je veux, Little John… et c\'est souvent dans la gueule.',
-      user: 'James Braddock'
+      user: 'James Braddock',
+      likers: ["Braddock", 'Billy']
     },
     {
       created_at: 'Thu Apr 06 15:24:15 +0000 2017',
       id: 2,
       text: 'Qui a deux pouces et qui s\'en fout ? Bob Kelso !',
-      user: 'Bob kelso'
+      user: 'Bob kelso',
+      likers: []
     },
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSave(newTweet) {
+    this.tweets.push({
+      created_at      : new Date(),
+      id              : this.tweets.length + 1,
+      text            : newTweet.value,
+      user            : this.user,
+      likers: []
+      });
+  }
+
+  onLike(tweet){
+    tweet.likers.includes(this.user);
+    tweet.favoriteCount += 1;
   }
 
 }
--- src/app/tweet.ts
+++ src/app/tweet.ts
@@ -1,2 +1,7 @@
 export interface Tweet {
+	created_at: string,
+  	id: number,
+  	text: string,
+  	user: string,
+	likers?: []
 }
